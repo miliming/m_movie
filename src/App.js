3a3f781bd98bbaad7b37602215f13dba7e6210ac
DESKTOP-A7B1IJA\ml <kmr1132@naver.com>
2022-04-12 22:47:15 +0900

    Pratice Clean up

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+    useEffect(function () {
+        console.log("hi :)");
+        return function () {
+          console.log("bye :(");
+        };
+      }, []);
+    return <h1>Hello</h1>
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1); //현재값에 +1
-  const onChange = (event) => setKeyword(event.target.value);
-  //console.log("i run all the time"); 
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);        // 처음에만 실행
-  useEffect(() => {
-      console.log("I run when 'keyword' changes.");
-  }, [keyword]);  // keyword가 변힐때만 실행
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);  // counter 변힐때만 실행
-  useEffect(() => {
-    console.log("I run when 'keyword' or 'counter' changes.");
-  }, [keyword,counter]);  //keywor 또는 counter 변힐때만 실행
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input 
-      value = {keyword}
-      onChange={onChange} 
-      type="text" 
-      placeholder="Search here"/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+        {showing ? <Hello /> : null}
+        <button onClick={onClick}>{showing? "Hide" : "Show"}</button>
     </div>
   );
 }
---
+++ src/App2.js
@@ -0,0 +1,35 @@
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1); //현재값에 +1
+  const onChange = (event) => setKeyword(event.target.value);
+  //console.log("i run all the time"); 
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);        // 처음에만 실행
+  useEffect(() => {
+      console.log("I run when 'keyword' changes.");
+  }, [keyword]);  // keyword가 변힐때만 실행
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);  // counter 변힐때만 실행
+  useEffect(() => {
+    console.log("I run when 'keyword' or 'counter' changes.");
+  }, [keyword,counter]);  //keywor 또는 counter 변힐때만 실행
+  return (
+    <div>
+      <input 
+      value = {keyword}
+      onChange={onChange} 
+      type="text" 
+      placeholder="Search here"/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
